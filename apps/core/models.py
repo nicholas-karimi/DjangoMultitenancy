832e10613a0e3a9a8bbdeaf581d5eea23afaaa6b
nicholas-karimi <kariminic@gmail.com>
2024-10-22 11:58:29 +0300

    add django colorfield models for site settings customization

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 
+from colorfield.fields import ColorField
+
 class Item(models.Model):
     name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+    
+
+class SiteSettings(models.Model):
+    name = models.CharField(max_length=255)
+    color = ColorField(default='#000000')
+    logo = models.ImageField(upload_to='logo/', null=True, blank=True)
 
     def __str__(self):
         return self.name
